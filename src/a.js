28ecb8275aad6c0c83e8aa566b553c3bcad237b9
rishhavv <43072384+rishhavv@users.noreply.github.com>
2020-01-09 23:36:18 +0530

    hhh
    jjjj

--- src/a.js
+++ src/a.js
@@ -1,4 +1,4 @@
-psa = require("pouchdb-seamless-auth")(PouchDB);
+var psa = require("pouchdb-seamless-auth")(PouchDB);
 const api= 'https://869a3a9a-8356-4ae9-8dbf-06e2f727e1ba-bluemix:76147209959e786263adc8636eb25e3e61edeb63e68d1b7aa0bd183690f2808f@869a3a9a-8356-4ae9-8dbf-06e2f727e1ba-bluemix.cloudantnosqldb.appdomain.cloud/';
 var db = new PouchDB(api.concat("school1"));
 
