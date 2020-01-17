18e4bfc150bd43d7c7cf423eff1d0ade61e431e1
rishhavv <43072384+rishhavv@users.noreply.github.com>
2020-01-17 16:57:09 +0530

    added authetication
    added authetication

--- src/schoolside/schoolsjs/login.js
+++ src/schoolside/schoolsjs/login.js
@@ -1,9 +1,21 @@
 function signup(){
-    var username= document.getElementById("schoolcode");
-    var password=document.getElementById("passwordschool");
+    var username= document.getElementById("schoolcode").value;
+    var password=document.getElementById("passwordschool").value;
     var db= new PouchDB("credentials");
     localStorage.setItem("username", username);
     localStorage.setItem("password", password);
-    window.location.href = "school_landing.html";
-    
-}
+   
+
+
+    axios.post('https://academicdb-api.herokuapp.com/api/users/login/', {
+        username: username,
+        password: password
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log(password)
+      });
+    }
