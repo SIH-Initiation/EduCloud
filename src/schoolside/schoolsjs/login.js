7e0b1c0da908eb20383b3612cc38ad17f13e1f17
rishhavv <43072384+rishhavv@users.noreply.github.com>
2020-01-18 16:27:15 +0530

    added offline suppoert for auth

--- src/schoolside/schoolsjs/login.js
+++ src/schoolside/schoolsjs/login.js
@@ -2,20 +2,64 @@ function signup(){
     var username= document.getElementById("schoolcode").value;
     var password=document.getElementById("passwordschool").value;
     var db= new PouchDB("credentials");
-    localStorage.setItem("username", username);
-    localStorage.setItem("password", password);
-   
-
+  var doc ={
+    '_id':username,
+    'password':password
+  }
+  db.get(username).then(function (doc) {
+      if(username["password"]==password){
+        console.log("abcd");
+        window.location.href='school_landing.html'
+      }
+  }).catch(function (err) {
+    console.log(err);
+    console.log("nhi milla")
+  });
 
     axios.post('https://academicdb-api.herokuapp.com/api/users/login/', {
         username: username,
         password: password
       })
       .then(function (response) {
+        db.put(doc).then(function (response) {
+          console.log(response);
+          console.log("ho gya")
+          }).catch(function (err) {
+            console.log(err);
+          });   
         console.log(response);
+        db.get(username).catch(function (err) {
+          if (err.name === 'not_found') {
+          console.log("not found, adding new");
+          db.put(doc).then(function (response) {
+           console.log(response);
+           }).catch(function (err) {
+             console.log(err);
+           });   
+          } else { 
+            console.log("kuch ho gya");
+            
+          }
+        }).then(function (configDoc) {
+          var doc2 = {
+            '_id':username,
+            'password':password,
+            "_rev":configDoc["_rev"] 
+          }
+          db.put(doc2).then(function (response) {
+            console.log(response);
+            console.log("ho gya")
+            }).catch(function (err) {
+              console.log(err);
+            });   
+            window.location.href='school_landing.html'
+
+        }).catch(function (err) {
+          console.error("err")
+        });
+        
       })
       .catch(function (error) {
         console.log(error);
-        console.log(password)
       });
     }
