54c199ca7ca6fba0a5df8c25be560522038c5210
rishhavv <43072384+rishhavv@users.noreply.github.com>
2020-01-19 01:24:31 +0530

    add student data updated

--- src/JS/student_data.js
+++ src/JS/student_data.js
@@ -1,27 +1,28 @@
 const api= 'https://869a3a9a-8356-4ae9-8dbf-06e2f727e1ba-bluemix:76147209959e786263adc8636eb25e3e61edeb63e68d1b7aa0bd183690f2808f@869a3a9a-8356-4ae9-8dbf-06e2f727e1ba-bluemix.cloudantnosqldb.appdomain.cloud/';
 
 function test (){
-  var sync = PouchDB.sync("school1",api.concat(localStorage.getItem("schoolcode")), {
+  var sync = PouchDB.sync("s1001",api.concat('s1001'), {
         live: true,
         retry: true
       }).on('change', function (info) {
         console.log("change chala");
       }).on('paused', function (err) {
-        // replication paused (e.g. replication up to date, user went offline)
+      
       }).on('active', function () {
-        // replicate resumed (e.g. new changes replicating, user went back online)
+       
       }).on('denied', function (err) {
-        // a document failed to replicate (e.g. due to permissions)
+       
       }).on('complete', function (info) {
        console.log("complete")
       }).on('error', function (err) {
-        // handle error
+       
       });
-  var db = new PouchDB("school1");
-  
-  
+  var db = new PouchDB("s1001");
+var str1 = "student:";
+var res = str1.concat(document.getElementById('admno').value);
+  console.log(res);
   var doc = {
-    '_id':  document.getElementById('admno').value,
+    '_id':  res,
     "name": document.getElementById("fname").value,
     'lastname': document.getElementById('lname').value,
     'schoolcode': document.getElementById('schoolcode').value,
@@ -38,16 +39,12 @@ function test (){
   
   }
   
-  // db.put(doc).then(function (response) {
-  //        prompt(response)
-  //       }).catch(function (err) {
-  //         console.log(err);
-  //       });   
+  
 
 
         db.get(document.getElementById('admno').value).catch(function (err) {
           if (err.name === 'not_found') {
-          console.log("not found, adding new");
+          console.log("res");
           db.put(doc).then(function (response) {
            console.log(response);
            }).catch(function (err) {
@@ -59,7 +56,7 @@ function test (){
           }
         }).then(function (configDoc) {
           var doc2 = {
-            '_id':  document.getElementById('admno').value,
+            '_id': res,
             "_rev":configDoc["_rev"],
             "name": document.getElementById("fname").value,
             'lastname': document.getElementById('lname').value,
