d1ee42616d0d4187c7e5033ab88d4fb8c6711cd4
rishhavv <43072384+rishhavv@users.noreply.github.com>
2020-01-18 23:57:54 +0530

    something something

--- src/JS/findschool.js
+++ src/JS/findschool.js
@@ -38,6 +38,7 @@ function findschools (){
 
     }).catch(function(err){
         document.getElementById("json").innerHTML=err
+        console.log("galat hua")
     });
     window.location.href='schoolprofile.html'
 
